e17a15c34e18e003bfe939b97e10ebbd2400da9c
MiladWaskut <mw1985by@gmail.com>
2018-05-10 02:56:38 +0300

    My homework for week 1 of javascript

---
+++ week_1_java.js
@@ -0,0 +1,102 @@
+let x;
+x=40;
+console.log("value of x is number 40");
+console.log(x);
+ console.log('the value of my string will be: A name');
+let y="Milad";
+console.log(y);
+y="Waskut";
+ console.log('the value of my string will be: A name');
+console.log(y);
+let z = 7.25;
+console.log(z);
+let a=7;
+console.log(a);
+
+let largest = Math.max(z, a);
+console.log(largest);
+let Animalarray = [];
+console.log('I GONNA NAME MY FAVOURITE ANIMALS');
+console.log(Animalarray);
+let Animalarray= ['Cat', 'dove', 'dolphin'];
+let daansFavoritAnimal = ['baby pig'];
+myfavouriteanimals.push(daansFavoritAnimal[0]);
+console.log(myfavouriteanimals);
+let myString = 'this is a test ';
+console.log(myString);
+console.log(myString.length);
+let number = 40;
+let string = 'Milad';
+let boolean = false;
+let array = ['Iran', 'Greece', 'Britain'];
+let digit=40;
+console.log('The value of  number is: ' + number);
+console.log('The value of  string is: ' + string);
+console.log('The value of  array is: ' +  array);
+console.log('The value of  boolean is: ' + boolean);
+console.log('The value of  digit is: ' + digit);
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof array);
+console.log(typeof boolean);
+console.log(typeof digit);
+if (number===string) {
+  console.log(number + "and" +string + "have the same type");
+}else {
+  console.log(number + "and" + string + "haven't the same value");
+}
+if (number===digit) {
+  console.log(number + "and" +digit + "have the same type");
+}else {
+  console.log(number + "and" + digit + "haven't the same value");
+}
+if (number===boolaen) {
+  console.log(number + "and" +boolean + "have the same type");
+}else {
+  console.log(number + "and" + boolean + "haven't the same value");
+}
+if (number===array) {
+  console.log(number + "and" +array + "have the same type");
+}else {
+  console.log(number + "and" + array + "haven't the same value");
+}
+if (string===boolean) {
+  console.log(string + "and" +boolean + "have the same type");
+}else {
+  console.log(string + "and" + boolean + "haven't the same value");
+}
+if (string===array) {
+  console.log(string + "and" +array + "have the same type");
+}else {
+  console.log(string + "and" + array + "haven't the same value");
+}
+if (array===boolean) {
+  console.log(array + "and" +boolean + "have the same type");
+}else {
+  console.log(array + "and" + boolean + "haven't the same value");
+}
+let x = 9;
+x = x % 3;
+console.log(x);
+let y = 13;
+y = y % 3;
+console.log(y);
+let z = 38;
+z = z % 14;
+console.log(z);
+let various = [3, false, 'text', 1, 'car', true];
+console.log(various);
+let dig1 = 6;//trying without /0
+let dig2 = 10;//trying without /0
+if (dig1 === dig2) {
+  console.log(" infinities are comparable");
+} else {
+  console.log("infinities aren't comparable");
+}
+ dig1 = 6/0;//trying with /0
+ dig2 = 10/0;//trying with /0
+if (dig1 === dig2) {
+  console.log(" infinities are comparable");
+} else {
+  console.log("infinities aren't comparable");
+}
